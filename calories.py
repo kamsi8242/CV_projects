187cf790554c8465b709f0962fbf0d45610e2c89
kamsi <kamsiokeke8242@gmail.com>
2020-06-13 15:28:39 +0100

    new commit

--- calories.py
+++ calories.py
@@ -1,43 +1,55 @@
-def calories():
 
-    from bs4 import BeautifulSoup
-    from selenium import webdriver
-    from urllib.request import urlopen
-    import requests
-    import string
-    # import re
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from urllib.request import urlopen
+import requests
+import string
+# import re
 
-    consuemd = input("Enter what you ate/drank: ")
-    query = consuemd.replace(' ', '+')
+num = int(input("how many foodstuffs did you have? \n"))
+count = 0
+daily_calories = []
+while count < num:
 
-    URL = 'https://www.nutracheck.co.uk/CaloriesIn/Product/Search?desc=' + query
-    User_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/72.0"
-    headers = {"user-agent" : User_agent}
-    resp = requests.get(URL, headers=headers)
-    soup = BeautifulSoup(resp.content, 'html.parser')
+    def calories():
 
-    text = soup.find_all('a', class_='textNoDecoration')
-    list = []
 
-    for details in text:
-        calorie_content = details.text
-        letters = str(string.ascii_letters)
-        characters = str(string.punctuation)
-        numbers = str(string.digits)
-        spaces = " "
-        newline = "\n "
-        combination = letters + characters + numbers + spaces
-        nutritional_details = calorie_content.lstrip(combination)
-        nutritional_details = nutritional_details.lstrip(newline)
-        nutritional_details2 = nutritional_details.rstrip(letters + numbers + ". ")
-        just_calories = nutritional_details2.rstrip(spaces + characters + letters)
-        list.append(int(just_calories))
 
-    global average_calories
-    average_calories = sum(list)/len(list)
-    
-    print("On average there are {0} calories contianed within a portion/serving of {1}.".format(average_calories, consuemd))
+            consuemd = input("Enter what you ate/drank: ")
+            query = consuemd.replace(' ', '+')
 
-if __name__ == "__main__":
-    
+            URL = 'https://www.nutracheck.co.uk/CaloriesIn/Product/Search?desc=' + query
+            User_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/72.0"
+            headers = {"user-agent" : User_agent}
+            resp = requests.get(URL, headers=headers)
+            soup = BeautifulSoup(resp.content, 'html.parser')
+
+            text = soup.find_all('a', class_='textNoDecoration')
+            list = []
+
+            for details in text:
+                calorie_content = details.text
+                letters = str(string.ascii_letters)
+                characters = str(string.punctuation)
+                numbers = str(string.digits)
+                spaces = " "
+                newline = "\n "
+                combination = letters + characters + numbers + spaces
+                nutritional_details = calorie_content.lstrip(combination)
+                nutritional_details = nutritional_details.lstrip(newline)
+                nutritional_details2 = nutritional_details.rstrip(letters + numbers + ". ")
+                just_calories = nutritional_details2.rstrip(spaces + characters + letters)
+                list.append(int(just_calories))
+
+            average_calories = sum(list)/len(list)
+            
+            print("On average there are {0} calories contianed within a portion/serving of {1}.".format(average_calories, consuemd))
+            global daily_calories
+            daily_calories.append(average_calories)
+
+        
     calories()
+
+    count += 1
+total_calories = sum(daily_calories)
+print("Current calorific intake is {0}".format(total_calories)) 
